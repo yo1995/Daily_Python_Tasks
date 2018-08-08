3eacff63cacb996225f131cf91d9a022ddcaec23
yo1995 <cht@bupt.edu.cn>
2018-08-08 14:13:05 +0800

    update

--- Downloader_imgfile_dot_net/ih-py3.py
+++ Downloader_imgfile_dot_net/ih-py3.py
@@ -6,6 +6,25 @@ import sys
 import urllib.request
 import time
 import random
+import multiprocessing
+
+
+def download_pic(url1, url2, url3, new_path2):
+    try:
+        content = urllib.request.urlopen(url1).read()
+    except Exception as e:
+        try:
+            content = urllib.request.urlopen(url2).read()
+        except Exception as e:
+            try:
+                content = urllib.request.urlopen(url3).read()
+            except Exception as e:
+                print(u'Not Found or Bad Request: ' + url0)
+                return False
+    with open(new_path2, 'wb') as saved_pic:
+        saved_pic.write(content)
+        return True
+
 
 # main
 if __name__ == '__main__':
@@ -26,6 +45,10 @@ if __name__ == '__main__':
         os.mkdir(new_path)
     url = re.findall(r'src="(.+?\.jpg)"', lines[count - 2], re.I)
     print('the total # of files is: ' + str(len(url)))
+    pool1 = multiprocessing.Pool(processes=4)
+    pool1._taskqueue._maxsize = 16  # maximum number of processes added into taskqueue
+    picture_download_timeout = 60
+
     for i in range(0, len(url)):
         try:
             url0 = url[i]
@@ -48,25 +71,32 @@ if __name__ == '__main__':
             picname = picname.group(0)
         new_path2 = new_path + picname
         if os.path.isfile(new_path2):
-            print('File already exist! Continue.')
+            print(i + 1, 'File already exist! Continue.')
             continue
         # time.sleep(random.random()) # to ensure that request is not too frequent
-        try:
-            content = urllib.request.urlopen(url1).read()
-        except Exception as e:
-            try:
-                content = urllib.request.urlopen(url2).read()
-            except Exception as e:
-                try:
-                    content = urllib.request.urlopen(url3).read()
-                except Exception as e:
-                    print(u'Not Found or Bad Request: ' + url0)
-                    continue
+        # try:
+        #     content = urllib.request.urlopen(url1).read()
+        # except Exception as e:
+        #     try:
+        #         content = urllib.request.urlopen(url2).read()
+        #     except Exception as e:
+        #         try:
+        #             content = urllib.request.urlopen(url3).read()
+        #         except Exception as e:
+        #             print(u'Not Found or Bad Request: ' + url0)
+        #             continue
+        #
+        # with open(new_path2, 'wb') as saved_pic:
+        #     saved_pic.write(content)
+        print('current downloading', i + 1)
+        pool1.apply_async(download_pic, args=(url1, url2, url3, new_path2))
+        # this part need to be polished.
+        # assert (download_status_flag)  # False abort the main program
 
-        with open(new_path2, 'wb') as saved_pic:
-            saved_pic.write(content)
-
+    pool1.close()
+    pool1.join()
     print('==============================')
     print(u'All Done!')
+
     sys.exit(0)
 

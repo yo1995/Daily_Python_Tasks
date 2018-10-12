14ec900e2bbd12d4d9dc4f909cecefc60897e2b3
yo1995 <cht@bupt.edu.cn>
2018-10-12 18:36:03 -0400

    add some dumpster

--- Dumpster_of_ideas/mp-read-write-lock-test-1.py
+++ Dumpster_of_ideas/mp-read-write-lock-test-1.py
@@ -14,8 +14,9 @@ def multi():
         dct = manager.dict({'a': 0, 'b': 0})
         pool = Pool()
         l = manager.Lock()
-        for i in range(1000):
+        for i in range(5000):
             pool.apply_async(test, args=(dct, l, ))
+            print(i)
 
         pool.close()
         pool.join()
@@ -25,7 +26,7 @@ def multi():
 
 def single():
     dct = dict({'a': 0, 'b': 0})
-    for i in range(1000):
+    for i in range(5000):
         dct['a'] += 1
         dct['b'] += 1
 
@@ -42,3 +43,5 @@ if __name__ == '__main__':
     b = single()
     print('single time used: ' + str(time.time() - t2))
     print('result is: ', b)
+
+

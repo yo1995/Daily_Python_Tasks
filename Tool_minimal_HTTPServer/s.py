1e4bb458b5de07707aaab2d9d101d05914af124e
yo1995 <cht@bupt.edu.cn>
2019-01-22 01:13:27 -0500

    wip http server

---
+++ Tool_minimal_HTTPServer/s.py
@@ -0,0 +1,36 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socketserver
+from sys import argv
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>".encode("utf-8"))
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        self._set_headers()
+        self.wfile.write("<html><body><h1>POST!</h1></body></html>".encode("utf-8"))
+        
+def run(server_class=HTTPServer, handler_class=S, port=9901):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
+        
